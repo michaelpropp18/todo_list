42b99c2e5fd81f1795597bb1f353dc6701de6f48
Michael Propp <michaelpropp8664@gmail.com>
2019-12-18 13:38:33 -0500

    Basic list functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems';
 
 class App extends React.Component{
+  state ={
+    items: [],
+    currentItem: {
+      text: '',
+      key: ''
+    }
+  }
+  handleInput = async (e) => {
+    this.setState({
+      currentItem : {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem = async (e) => {
+    e.preventDefault()
+    const newItem = this.state.currentItem;
+    if (newItem.text !== '') {
+      const newItems = [...this.state.items, newItem]
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
   render() {
     return (
       <div className='App'>
         <header>
-          <form id='to-do-form'>
-            <input type='text' placeholder='enter text'/>
-            <button type='submit'>Add </button>
+          <form id='to-do-form' onSubmit={this.addItem}>
+            <input 
+              type='text' 
+              placeholder='enter text' 
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}/>
+            <button type='submit'>Add</button>
           </form>
         </header>
+        <ListItems items={this.state.items}></ListItems>
       </div>
     );
   }
