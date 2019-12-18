5f6c49e6e3fdbe26c06d25466ac99f4787647321
Michael Propp <michaelpropp8664@gmail.com>
2019-12-18 14:41:33 -0500

    Updates

---
+++ precache-manifest.e97c1ee4ab95fd7cd46dd62f851d5bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86135121828c85fba3c582b92c7581c",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "1ed2060e6e49cc85a27f",
+    "url": "/todo_list/static/css/main.e8f9d929.chunk.css"
+  },
+  {
+    "revision": "0c2fc6821bb56bfd9a11",
+    "url": "/todo_list/static/js/2.926bcb52.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo_list/static/js/2.926bcb52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ed2060e6e49cc85a27f",
+    "url": "/todo_list/static/js/main.e7b9f709.chunk.js"
+  },
+  {
+    "revision": "a77965bb83b1883c5d87",
+    "url": "/todo_list/static/js/runtime-main.c5c93c58.js"
+  }
+]);
